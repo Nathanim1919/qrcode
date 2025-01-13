fa0af0d5ec7f707eaa1c39838aea0977e2abdac1
Nathanim1919 <nathanim2tadele@gmail.com>
2025-01-13 15:27:52 +0300

    Add HomePage, AdminPage, and LoginPage routes to the application

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,17 @@
 import "./App.css";
 import { ValidateQrCode } from "./components/validateQrCode";
 import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/Home";
+import AdminPage from "./pages/AdminPage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
     <Routes>
-      
-      <Route path="/validateQrCode" element={<ValidateQrCode/>} />
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/admin" element={<AdminPage />} />
+      <Route path="/validateQrCode" element={<ValidateQrCode />} />
     </Routes>
   );
 }
