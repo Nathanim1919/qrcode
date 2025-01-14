57f12cadbda2d4196c06a1d7bf7cf60d5744cd0f
Nathanim1919 <nathanim2tadele@gmail.com>
2025-01-14 11:18:22 +0300

    Add scanTime parameter to QR code validation; update validation logic to include scan time

--- server/src/controller/qrcode.controller.js
+++ server/src/controller/qrcode.controller.js
@@ -19,7 +19,7 @@ export const generateQrCode = async (req, res) => {
 
 export const validateQrCode = async (req, res) => {
   try {
-    const { qrcodeId, scanType } = req.query;
+    const { qrcodeId, scanType, scanTime } = req.query;
 
 
     if (!qrcodeId) {
@@ -28,7 +28,7 @@ export const validateQrCode = async (req, res) => {
         .json({ success: false, message: "qrcodeId is required" });
     }
 
-    const response = await validateQrCodeForEvent(qrcodeId, scanType);
+    const response = await validateQrCodeForEvent(qrcodeId, scanType, scanTime);
     return res.status(200).json({ success: response.success, message: response.message });
   } catch (error) {
     let statusCode = 500;
